1e730488c0b871c391d01d6a23d55f9c41daf34f
Rupesh Kumar <rupeshbgp0607@gmail.com>
2024-12-30 12:18:34 +0530

    function in JS

---
+++ Basic/functions.js
@@ -0,0 +1,20 @@
+// Function Statement
+
+function a(){
+    console.log("A");
+}
+
+// Function Expression
+
+var b=function(){
+    console.log("B");
+}
+
+// Named function Expression
+
+var c=function cat(){
+    console.log("Cat");
+}
+
+c();
+// cat();  not be used like this
--- Basic/index.js
+++ Basic/index.js
@@ -9,6 +9,64 @@ function a(){
 }
 
 a();
+
+// example of closure
+
+function outer(){
+    var a=12,b=8;
+    function inner(){
+        console.log(a);
+    }
+    return inner;
+}
+
+outer()();// this is simillar to var ans=outer(); the run ans();
+
+// advantage of closure
+// 1. encapsulation and data hiding
+// example
+console.log(a);
+// console.log(b);  b is not defined here
+
+function Counter(){
+    var count=0;
+    console.log(count);
+    this. increaseCount=function (){
+        count++;
+        console.log(count);
+    }
+
+    this. decreaseCount=function (){
+        count--;
+        console.log(count);
+    }
+}
+
+Counter();  //it work as a function
+
+var count1=new Counter();  //it work as an object whe we use new keyword
+count1.increaseCount();
+count1.increaseCount();
+count1.increaseCount();
+count1.decreaseCount();
+count1.decreaseCount();
+count1.increaseCount();
+count1.increaseCount();
+
+
+document.getElementById("clickme").addEventListener("click",function btn(){
+
+    console.log("Button clicked");
+});
+
+
+
+
+
+
+
+
+
     
 
 
